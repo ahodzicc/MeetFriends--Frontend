22a793e8f100fb98441213c4fc27a18d790e77dc
dzani <dzanieterle531@gmail.com>
2024-04-08 00:15:23 +0200

    merge test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,29 +4,15 @@ import {HomepageComponent} from "./homepage/homepage.component";
 import {MeetUpConfirmationComponent} from "./meet-up-confirmation/meet-up-confirmation.component";
 import {LoginComponent} from "./login/login.component";
 import {SignupComponent} from "./signup/signup.component";
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomepageComponent} from "./homepage/homepage.component";
-import {MeetUpRequestComponent} from './meet-up-request/meet-up-request.component';
-import {PendingMeetupRequestsComponent} from './pending-meetup-requests/pending-meetup-requests.component';
-import {MeetUpConfirmationComponent} from "./meet-up-confirmation/meet-up-confirmation.component";
 
 const routes: Routes = [
   {
-    path: "home",
+    path: "H",
     component: HomepageComponent
   },
   {
-    path: "meetup",
-    component: MeetUpConfirmationComponent
-  },
-  {
-    path: "request",
-    component: MeetUpRequestComponent
-  },
-  {
-    path: "pending",
-    component: PendingMeetupRequestsComponent
+  path: "M",
+  component: MeetUpConfirmationComponent
   },
   {
     path: "",
@@ -43,5 +29,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-
